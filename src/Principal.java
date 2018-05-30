387a9cd8fce649464773c05357b31500eef19439
unknown <luisflv@outlook.com>
2018-05-30 13:59:45 -0600

    Subiendo Proyecto

---
+++ src/Principal.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JOptionPane;
+
+
+public class Principal {
+
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Proyecto");
+      
+    }
+    
+}
